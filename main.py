05bfd7b506f34ff719cf5f387eadbaf5bfe3c2c4
lukas <s24729@pjwstk.edu.pl>
2024-06-18 18:55:18 +0200

    Added switching between sorting and filtering options

--- main.py
+++ main.py
@@ -13,6 +13,26 @@ def show_results():
     tk.Label(root, text="Not implemented yet").pack(padx=20, pady=20)
 
 
+def match_options_for_operations(event):
+    selected_operation = operation_combobox.get()
+
+    match selected_operation:
+        case "Filter":
+            order_label.grid_forget()
+            order_combobox.grid_forget()
+            value_label.grid(row=3, column=0)
+            value_entry.grid(row=3, column=1, padx=10, pady=10)
+        case "Sort":
+            if not order_label.winfo_viewable():
+                order_label.grid(row=3, column=0)
+            if not order_combobox.winfo_viewable():
+                order_combobox.grid(row=3, column=1, padx=10, pady=10)
+            if value_label.winfo_viewable():
+                value_label.grid_forget()
+            if value_entry.winfo_viewable():
+                value_entry.grid_forget()
+
+
 # Pandas view configuration
 pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
@@ -24,28 +44,38 @@ players_datasource = datasource_directory + 'players.csv'
 root = tk.Tk()
 root.title("Football data analysis project")
 
-tk.Label(root, text="Operation:").grid(row=0, column=0)
+operation_label = tk.Label(root, text="Operation:")
+operation_label.grid(row=0, column=0)
 operation_combobox = ttk.Combobox(root, values=["Sort", "Filter"])
 operation_combobox.grid(row=0, column=1, padx=10, pady=10)
 operation_combobox.current(0)
 
-tk.Label(root, text="Target:").grid(row=1, column=0)
+operation_combobox.bind("<<ComboboxSelected>>", match_options_for_operations)
+
+target_label = tk.Label(root, text="Target:")
+target_label.grid(row=1, column=0)
 target_combobox = ttk.Combobox(root, values=["Players", "Clubs"])
 target_combobox.grid(row=1, column=1, padx=10, pady=10)
 target_combobox.current(0)
 
-tk.Label(root, text="By:").grid(row=2, column=0)
-target_combobox = ttk.Combobox(root, values=["Name"])
-target_combobox.grid(row=2, column=1, padx=10, pady=10)
-target_combobox.current(0)
+by_label = tk.Label(root, text="By:")
+by_label.grid(row=2, column=0)
+by_combobox = ttk.Combobox(root, values=["Name"])
+by_combobox.grid(row=2, column=1, padx=10, pady=10)
+by_combobox.current(0)
 
-tk.Label(root, text="Order:").grid(row=3, column=0)
-target_combobox = ttk.Combobox(root, values=["Ascending", "Descending"])
-target_combobox.grid(row=3, column=1, padx=10, pady=10)
-target_combobox.current(0)
+order_label = tk.Label(root, text="Order:")
+order_label.grid(row=3, column=0)
+order_combobox = ttk.Combobox(root, values=["Ascending", "Descending"])
+order_combobox.grid(row=3, column=1, padx=10, pady=10)
+order_combobox.current(0)
+
+value_label = tk.Label(root, text="Value")
+value_label.grid_forget()
+value_entry = tk.Entry(root)
+value_entry.grid_forget()
 
 tk.Button(root, text="Analyze", command=show_results).grid(row=4, column=0, padx=10, pady=10)
-tk.Button(root, text="Clear window", command=clear_window).grid(row=4, column=1, padx=10, pady=10)
 
 root.mainloop()
 
