070e653ed821e2ae50f97f3dfe0c8f953474df8d
s24729-pj <s24729@pjwstk.edu.pl>
2024-06-19 17:55:52 +0200

    Added sorting players by name

--- main.py
+++ main.py
@@ -3,32 +3,44 @@ import tkinter as tk
 from tkinter import ttk
 
 
-global results_df
-
 def clear_window():
     for widget in root.winfo_children():
         widget.destroy()
 
 
 def show_results():
-    results_df = sort(target_combobox.get(), by_combobox.get(), order_combobox.get())
-    print(results_df)
+    operation = operation_combobox.get()
+    target = target_combobox.get()
+    by = by_combobox.get()
+    order = order_combobox.get()
+
     clear_window()
-    print(results_df)
-    # print(results_df)
-    match operation_combobox.get():
+
+    frame = ttk.Frame(root)
+    frame.pack(expand=True, fill='both')
+
+    results_df = pd.DataFrame()
+
+    match operation:
         case "Sort":
-            print(results_df)
-            # results_df = sort(target_combobox.get(), by_combobox.get(), order_combobox.get())
-            results_view = ttk.Treeview(root)
-            results_view['columns'] = list(results_df.columns)
-            results_view['show'] = 'headings'
-            for column in results_df.columns:
-                results_view.heading(column, text=column)
-                results_view.column(column, anchor="w")
-            for index, row in results_df.iterrows():
-                results_view.insert("", "end", values=list(row))
-            results_view.pack(expand=True, fill='both')
+            results_df = sort(target, by, order)
+
+    results_view = ttk.Treeview(root)
+    results_view['columns'] = list(results_df.columns)
+    results_view['show'] = 'headings'
+
+    for column in results_df.columns:
+        results_view.heading(column, text=column)
+        results_view.column(column, anchor="w")
+    for index, row in results_df.iterrows():
+        results_view.insert("", "end", values=list(row))
+
+    h_scrollbar = ttk.Scrollbar(frame, orient="horizontal", command=results_view.xview)
+    h_scrollbar.pack(side="bottom", fill="x")
+    results_view.configure(xscrollcommand=h_scrollbar.set)
+    results_view.pack(side="top", fill="both", expand=True)
+
+    print(results_df)
 
 
 def sort(target, by, order):
@@ -110,58 +122,3 @@ value_entry.grid_forget()
 tk.Button(root, text="Analyze", command=show_results).grid(row=4, column=0, padx=10, pady=10)
 
 root.mainloop()
-
-# def show_available_operations():
-#     print()
-#     print("Available operations:")
-#     print("1d. Sort players by their market value - descending")
-#     print("1a. Sort players by their market value - ascending")
-#     print("2nm. Filter players by their name")
-#     print("2c. Filter players by clubs")
-#     print("2nt. Filter players by nationality")
-#     print("0. Exit")
-#     print()
-#
-# print()
-# print("Football data analysis project")
-# print("------------------------------")
-#
-# df = pd.read_csv(players_datasource)
-#
-# show_available_operations()
-#
-# selection = ''
-#
-# while True:
-#     selection = input("Choose operation (L for list of available operations): ")
-#
-#     match selection:
-#         case '1d':
-#             print()
-#             print(df.sort_values('market_value_in_eur', ascending=False))
-#             show_available_operations()
-#         case '1a':
-#             print()
-#             print(df.sort_values('market_value_in_eur', ascending=True))
-#             show_available_operations()
-#         case '2nm':
-#             searched = input("Name: ")
-#             print()
-#             print(df.loc[df['name'] == searched])
-#             show_available_operations()
-#         case '2c':
-#             searched = input("Club name: ")
-#             print()
-#             print(df.loc[df['current_club_name'] == searched])
-#             show_available_operations()
-#         case '2nt':
-#             searched = input("Nationality: ")
-#             print()
-#             print(df.loc[df['country_of_citizenship'] == searched])
-#             show_available_operations()
-#         case '0':
-#             exit(0)
-#         case 'L' | 'l':
-#             show_available_operations()
-#         case _:
-#             print("Invalid selection")
